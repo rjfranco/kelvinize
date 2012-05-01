fb70e9457dc6f463fbbd7371e6c93ce0352c372b
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-04-30 17:40:53 -0700

    Adding math functions to get proper kelvin, now I just need to find out how to truc / round the decimal to 1 place.

--- kelvinize/main.c
+++ kelvinize/main.c
@@ -10,9 +10,19 @@
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    double fahrenheit;
+    double kelvin;
+    
+    if( argc > 1 ) {
+        fahrenheit = atof(argv[1]);
+    } else {
+        fahrenheit = 0;
+    }
+    
+    kelvin = (((fahrenheit - 32) * 5) / 9) + 273.15;
+    
+    
+    printf("%f Fahrenheit is %f Kelvin\n", fahrenheit, kelvin);
     return 0;
 }
 
